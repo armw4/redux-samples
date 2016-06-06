f1e955e5f2d4520d1d4b53354cd6fc67443865ae
Antwan Wimberly <antwan.wimberly@gmail.com>
2016-06-05 17:08:06 -0700

    localize the components

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import './style.css'
-import UPCA from './apps/upc-a'
+import UPCA from './apps/upc-a/components'
 
 const mountNode = document.getElementById('content')
 
