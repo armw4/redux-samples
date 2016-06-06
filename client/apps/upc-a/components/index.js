b9694a51a262b73bca3f32fe32ed8744cb0fa7f7
Antwan Wimberly <antwan.wimberly@gmail.com>
2016-06-06 13:36:05 -0700

    Add static property support and prop type baseline

--- client/apps/upc-a/components/index.js
+++ client/apps/upc-a/components/index.js
@@ -1,6 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
 import style from './style.css'
 
-const Container = () => <div className={style.root}>Hello @wacky-world.net</div>
+const PRODUCT_CODE_SHAPE = PropTypes.shape({
+  value: PropTypes.string
+})
 
-export default Container
+class Container extends Component {
+  static propTypes = {
+    productCodes: PropTypes.arrayOf(PRODUCT_CODE_SHAPE).isRequired,
+    productCodesInvalid: PropTypes.bool.isRequired
+  }
+
+  render () {
+    return <div className={style.root}>Hello @wacky-world.net</div>
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    productCodes: state.productCodes,
+    productCodesInvalid: true
+  }
+}
+
+export default connect(mapStateToProps)(Container)
---
+++ client/apps/upc-a/reducers/product-codes.js
@@ -0,0 +1,8 @@
+const DEFAULT_STATE = [{ value: null }]
+
+export default (state = DEFAULT_STATE, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
